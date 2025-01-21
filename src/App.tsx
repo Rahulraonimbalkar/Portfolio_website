83be295b560d707483279a1d68c7a3e38a6e8917
Rahul nimbalkar <117708809+Rahulraonimbalkar@users.noreply.github.com>
2025-01-21 11:41:38 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
           </div>
           <div className="md:w-1/2 mt-8 md:mt-0">
             <img 
-              src="src/profile-pic.png"
+              src="/profile-pic.png"
               className="rounded-full w-64 h-64 object-cover mx-auto border-4 border-purple-500"
             />
           </div>
