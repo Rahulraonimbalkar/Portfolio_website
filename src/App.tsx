f0dee6670ec466aec930fb155a8feb7b1df5a2ce
Rahul nimbalkar <117708809+Rahulraonimbalkar@users.noreply.github.com>
2025-01-21 11:36:26 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
           <div className="md:w-1/2 mt-8 md:mt-0">
             <img 
               src="src/profile-pic (1).png" 
-              alt="Rahul Nimbalkar"
+{/*               alt="Rahul Nimbalkar" */}
               className="rounded-full w-64 h-64 object-cover mx-auto border-4 border-purple-500"
             />
           </div>
